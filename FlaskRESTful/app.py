67314ec2da7e399ef61be8939afac88acb98a32f
Brandon Teo <brandonjwteo@gmail.com>
2018-07-16 17:49:09 -0700

    [FlaskRESTful] Enabled reqparse and added user authentication.

--- FlaskRESTful/app.py
+++ FlaskRESTful/app.py
@@ -1,22 +1,55 @@
 from flask import Flask
 from flask import request # This import lets us access the request of the incoming API calls
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
+from flask_jwt import JWT, jwt_required
 
 app = Flask(__name__)
+app.secret_key = "*T87&*(g99*H"
 api = Api(app)
 
 # Temporary data structure to store our items
 inven = []
 
+# AUTH CODE ---------------------------------------------
+class User:
+    def __init__(self, _id, username, pw):
+        self.id = _id
+        self.username = username
+        self.password = pw
+
+users = [User(1, 'brandon', 'password')]
+username_table = {u.username: u for u in users}
+userid_table = {u.id: u for u in users}
+
+def authenticate(un, pw):
+    user = username_table.get(un, None)
+    if user and user.password == pw:
+        return user
+
+def identity(payload):
+    userid = payload['identity']
+    user = userid_table.get(userid, None)
+    return user
+
+jwt = JWT(app, authenticate, identity)
+# -------------------------------------------------------
+
+
 # # A `Resource` is one that we can perform CRUD operations on in a RESTful scheme
 class Inventory(Resource):
+    # Initialize a parser for this resource
+    parser = reqparse.RequestParser()
+    parser.add_argument('name', type=str, required=True)
+    parser.add_argument('price', type=float, required=True)
+    
     # INDEX route
     def get(self):
         return {'allitems': inven}
 
     # CREATE route
+    @jwt_required()
     def post(self):
-        body = request.get_json()
+        body = Inventory.parser.parse_args()
         item = next(filter(lambda x: x['name'] == body['name'], inven), None)
 
         if item:
@@ -26,6 +59,12 @@ class Inventory(Resource):
             return "Added item to our inventory.", 201
 
 class Item(Resource):
+    # Initialize a parser for this resource
+    parser = reqparse.RequestParser()
+    # Notice that for this parser we don't want `name` since we don't want to allow updating
+    # of the field `name` and only `price`
+    parser.add_argument('price', type=float)
+
     # SHOW route
     def get(self, item_name):
         item = next(filter(lambda x: x['name'] == item_name, inven), None)
@@ -35,8 +74,9 @@ class Item(Resource):
             return "This item does not exist -> unable to show.", 400
 
     # UPDATE route
+    @jwt_required()
     def put(self, item_name):
-        body = request.get_json()
+        body = Item.parser.parse_args()
         item = next(filter(lambda x: x['name'] == item_name, inven), None)
         if item:
             item.update(body)
@@ -45,6 +85,7 @@ class Item(Resource):
             return "This item does not exist -> unable to update.", 400
 
     # DESTROY route
+    @jwt_required()
     def delete(self, item_name):
         global inven # Need to do this if we're assigning smth to it
 
@@ -59,4 +100,4 @@ class Item(Resource):
 api.add_resource(Inventory, '/')
 api.add_resource(Item, '/<string:item_name>')
 
-app.run(port=5000)
+app.run(port=5000, debug=True)
